6898808df30bf7615c902963d9d0a5ea2a8e3872
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-04-24 18:15:10 +0800

    优化注解

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
     public String testHello() {
         return "Hello Spring Boot!";
     }
