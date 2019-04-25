53450b9995c344105cf4a767165202d6395cd643
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-04-25 18:21:09 +0800

    改为 HTTPS 访问方式

---
+++ src/main/java/com/example/demo/config/TomcatConfig.java
@@ -0,0 +1,49 @@
+package com.example.demo.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 将 HTTP 请求重定向为 HTTPS
+ * 配置一个 TomcatServletWebServerFactory, 然后添加一个 Tomcat 中的 Connector, 并将请求转发到 8443 上
+ *
+ * @author 郑智国 (zhiguo.zheng@ucarinc.com)
+ * @time 2019/4/25 18:00
+ */
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(createTomcatConnector());
+        return factory;
+    }
+
+    private Connector createTomcatConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
