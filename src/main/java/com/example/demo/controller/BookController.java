a8d1f7c5720fdc5fb6cd4628bda1486b8165ba6d
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-04-28 23:35:16 +0800

    类型安全配置属性

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 书籍 Controller
+ *
+ * @author zhiguo.zheng
+ * @time 2019/4/28 23:20
+ */
+@RestController
+public class BookController {
+
+    @Autowired
+    private Book book;
+
+    @GetMapping(path = {"/book"})
+    public String book() {
+        return book.toString();
+    }
+}
