4ab0591a2e2805661659cb17e00a538b7e9cc227
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-04-30 16:19:39 +0800

    两种方式 配置静态资源访问

---
+++ src/main/java/com/example/demo/config/MyWebMvcConfig.java
@@ -0,0 +1,22 @@
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * 自定义静态资源访问
+// *
+// * @author 郑智国 (zhiguo.zheng@ucarinc.com)
+// * @time 2019/4/30 16:15
+// */
+//@Configuration
+//public class MyWebMvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/static/");
+//    }
+//}
