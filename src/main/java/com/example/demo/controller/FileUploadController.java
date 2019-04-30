595065d0241fec0529ad80dd8aeb757f325cb48c
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-04-30 18:37:45 +0800

    单文件上传

---
+++ src/main/java/com/example/demo/controller/FileUploadController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * 文件上传 Controller
+ *
+ * @author 郑智国 (zhiguo.zheng@ucarinc.com)
+ * @time 2019/4/30 17:02
+ */
+@RestController
+public class FileUploadController {
+
+    // 日期格式
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+    @PostMapping(path = "/upload")
+    public String upload(MultipartFile uploadFile, HttpServletRequest request) {
+
+        String realPath = "E:\\Project_Zheng\\springboot-demo\\src\\main\\resources\\static\\uploadFile\\";
+        String format = sdf.format(new Date());
+
+        // 根据日期创建文件路径
+        File folder = new File(realPath + format);
+        // 路径不存在时创建目录
+        if (!folder.isDirectory()) {
+            folder.mkdirs();
+        }
+        // 获取原文件名
+        String oldName = uploadFile.getOriginalFilename();
+        // 重命名文件名
+        String newName = UUID.randomUUID().toString() + oldName.substring(oldName.lastIndexOf("."), oldName.length());
+        try {
+            uploadFile.transferTo(new File(folder, newName));
+            String filePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/static/uploadFile/" + format + "/" + newName;
+            return filePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+}
