c2fe186cd9033349a32a4f3e36160c96cbad7410
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-05-08 18:59:34 +0800

    全局异常处理

---
+++ src/main/java/com/example/demo/exception/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.demo.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 全局异常处理
+ *
+ * @author 郑智国 (zhiguo.zheng@ucarinc.com)
+ * @time 2019/5/8 18:43
+ */
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public void uploadException(MaxUploadSizeExceededException e, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.write("上传文件大小超出限制");
+        out.flush();
+        out.close();
+    }
+}
